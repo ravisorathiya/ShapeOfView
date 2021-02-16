8aace770c61e739ed327e97600e218c19d118d6e
Ravi <ravisorathiya508@gmail.com>
2021-02-16 17:26:20 +0530

    backgrond color set option menu

---
+++ app/src/main/java/com/example/shapeolorpp/ui/BottomShitHorizontalFragment.kt
@@ -0,0 +1,60 @@
+package com.example.shapeolorpp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shapeolorpp.R
+import com.example.shapeolorpp.adapters.BodyHorizontalAdapter
+import com.example.shapeolorpp.models.BodyHorizontalData
+import com.example.shapeolorpp.utills.BodyHorizontal
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomShitHorizontalFragment : BottomSheetDialogFragment()  , BodyHorizontalAdapter.OnBodyColorClick {
+    private lateinit var listBodyColor : ArrayList<BodyHorizontalData>
+
+
+
+    lateinit var viewLayout: ConstraintLayout
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_bodyhorizontal_color,container,false)
+
+
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecylcleBodyColor()
+    }
+
+    private fun setupRecylcleBodyColor() {
+        val adapterBodyColor = BodyHorizontalAdapter(this)
+        val rvBody = view?.findViewById<RecyclerView>(R.id.rv_back_color)
+        listBodyColor = BodyHorizontal.colorCollector()
+        rvBody?.setHasFixedSize(true)
+        rvBody?.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,true)
+        adapterBodyColor.submitList(listBodyColor)
+        rvBody?.adapter = adapterBodyColor
+    }
+
+    override fun changeBodyColor(position: Int) {
+
+        viewLayout = requireActivity().findViewById(R.id.const_layout)
+        listBodyColor.forEachIndexed { index, backColorData ->
+            if (position == index){
+                val data = backColorData.radialDrawable
+                viewLayout.background = data
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/shapeolorpp/ui/BottomShitVerticleFragment.kt
@@ -0,0 +1,60 @@
+package com.example.shapeolorpp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shapeolorpp.R
+import com.example.shapeolorpp.adapters.BodyVerticleAdapter
+import com.example.shapeolorpp.models.BodyVerticleData
+import com.example.shapeolorpp.utills.BodyVerticle
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomShitVerticleFragment : BottomSheetDialogFragment() ,  BodyVerticleAdapter.VerticleColorSet {
+    private lateinit var listVerticleColor: ArrayList<BodyVerticleData>
+    lateinit var viewLayout: ConstraintLayout
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_bodyverticle_color,container,false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecylcleBodyColor()
+    }
+
+
+    private fun setupRecylcleBodyColor() {
+        val rvVerticle = view?.findViewById<RecyclerView>(R.id.rv_verticle_body_color)
+        val verticleAdapter = BodyVerticleAdapter(this)
+        rvVerticle?.setHasFixedSize(true)
+        listVerticleColor = BodyVerticle.verticleColor()
+        BodyVerticleAdapter(this)
+        verticleAdapter.submitList(listVerticleColor)
+        rvVerticle?.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, true)
+        rvVerticle?.adapter = verticleAdapter
+    }
+
+    override fun onVerticle(position: Int) {
+        viewLayout = requireActivity().findViewById(R.id.const_layout)
+        listVerticleColor.forEachIndexed { index, bodyVerticleColorData ->
+
+            if (position == index) {
+                val data = bodyVerticleColorData.radialDrawable
+                viewLayout.background = data
+            }
+
+        }
+    }
+}
